43d81b50dd91914f32fee4a1ff4345424a0d40ff
Chandan Dwivedi <chhandan.dwivedi99@gmail.com>
2018-08-21 11:22:33 +0530

    update

--- gpu-setup-part1.sh
+++ gpu-setup-part1.sh
@@ -1,4 +1,4 @@
-#!/bin/bash 
+#!/bin/bash
 
 #if [ "$EUID" -ne 0 ]; then
 #	echo "Please run as root (with sudo)"
@@ -10,17 +10,16 @@ mkdir -p $SETUP_DIR
 cd $SETUP_DIR
 
 # install python libraries
-sudo apt-get -y install python-numpy python-dev python-wheel python-mock python-matplotlib python-pip
+sudo apt-get -y install python3-numpy python3-dev python3-wheel python3-mock python3-matplotlib python3-pip
 
 # install cuda drivers
-if [ ! -f "cuda_8.0.61_375.26_linux-run" ]; then 
-	wget https://developer.nvidia.com/compute/cuda/8.0/Prod2/local_installers/cuda_8.0.61_375.26_linux-run
+if [ ! -f "cuda_9.2.148_396.37_linux.run" ]; then
+	echo "please visit https://developer.nvidia.com/cuda-downloads?target_os=Linux&target_arch=x86_64&target_distro=Ubuntu&target_version=1710&target_type=runfilelocal to download .run file of cuda."
 fi
-chmod +x cuda_8.0.61_375.26_linux-run
-sudo sh cuda_8.0.61_375.26_linux-run --silent --verbose --driver
+chmod +x cuda_9.2.148_396.37_linux.run
+sudo sh cuda_9.2.148_396.37_linux.run --silent --verbose --driver
 
 echo "Setup requires a reboot to continue."
 echo "The VM will reboot now. Login after it restarts and continue installation from part2."
 
-sudo reboot
-
+#sudo reboot
