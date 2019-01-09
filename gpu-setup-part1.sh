18dfa91ca42ea2af17c676e597ac864c86c13a39
Victor Khaustov <3192677+vi3itor@users.noreply.github.com>
2019-01-09 10:17:38 +0900

    Back to Cuda 9.0 (part 1)

--- gpu-setup-part1.sh
+++ gpu-setup-part1.sh
@@ -15,12 +15,14 @@ sudo apt-get -y update
 # install python libraries
 sudo apt-get -y install python3-numpy python3-dev python3-wheel python3-mock python3-matplotlib python3-pip
 
+https://developer.nvidia.com/compute/cuda/9.0/Prod/local_installers/cuda_9.0.176_384.81_linux-run
+
 # install cuda drivers
-if [ ! -f "cuda_10.0.130_410.48_linux.run" ]; then
-	echo "please visit https://developer.nvidia.com/cuda-downloads?target_os=Linux&target_arch=x86_64&target_distro=Ubuntu&target_type=runfilelocal to download .run file of cuda."
+if [ ! -f "cuda_9.0.176_384.81_linux-run" ]; then
+	wget https://developer.nvidia.com/compute/cuda/9.0/Prod/local_installers/cuda_9.0.176_384.81_linux-run	
 fi
-chmod +x cuda_10.0.130_410.48_linux.run
-sudo sh cuda_10.0.130_410.48_linux.run --silent --verbose --driver
+chmod +x cuda_9.0.176_384.81_linux-run
+sudo sh cuda_9.0.176_384.81_linux-run --silent --verbose --driver
 
 echo "Setup requires a reboot to continue."
 echo "The VM will reboot now. Login after it restarts and continue installation from part2."
